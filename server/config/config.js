a453179381a548fd4611a1ebaecdd10d179cad06
David Rodarte <drh697@gmail.com>
2020-04-30 09:21:54 -0500

    Variable de entorno MONGO_URI

--- server/config/config.js
+++ server/config/config.js
@@ -8,13 +8,15 @@ process.env.PORT = process.env.PORT || 3000;
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 /**
- * Bases de datos
+ * Bases de datos,
+ * definimos una variable de entorno MONGO_URI en caso de que
+ * la conexión sea a una base de datos en producción
  */
 
  if(process.env.NODE_ENV === 'dev'){
      urlDB = 'mongodb://localhost:27017/cafe';
  }else{
-     urlDB = 'mongodb+srv://drodarte:hrk34x9ogxqOT6db@cluster0-h2kzn.mongodb.net/cafe';
+     urlDB = process.env.MONGO_URI;
  }
 
  /**
